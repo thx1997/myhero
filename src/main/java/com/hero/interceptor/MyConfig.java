602983426777990d30e17e4127f34eefdc61fb06
lenovo <lenovo@LAPTOP-UBESI9S8>
2018-12-05 19:20:26 +0800

    init files

--- src/main/java/com/hero/interceptor/MyConfig.java
+++ src/main/java/com/hero/interceptor/MyConfig.java
@@ -4,16 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+/**
+ * 
+ * @author public
+ *
+ */
 @SpringBootApplication//表明这是一个配置类
 public class MyConfig extends WebMvcConfigurerAdapter{
 	@Autowired
-	private MyInterceptor myInterceptor;
+	private LoginInterceptor loginInterceptor;
 	/**
 	 * 自定义拦截器
 	 * 继承WebMvcConfigurerAdapter,重写addInterceptors方法
 	 */
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+		registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
 	}
 }
