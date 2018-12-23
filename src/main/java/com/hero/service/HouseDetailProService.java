3b94c1d9b900073abd4bd3858cd3ebe9d1e3f3a9
lenovo <lenovo@LAPTOP-UBESI9S8>
2018-12-23 18:22:18 +0800

    init

--- src/main/java/com/hero/service/HouseDetailProService.java
+++ src/main/java/com/hero/service/HouseDetailProService.java
@@ -1,19 +1,13 @@
-package com.hero.repository;
+package com.hero.service;
 
 import java.util.List;
 
 import com.hero.entity.HouseDetailPro;
 import com.hero.entity.query.HouseDetailProQuery;
 
-public interface HouseDetailProMapper {
-    int insert(HouseDetailPro record);
-
-    int insertSelective(HouseDetailPro record);
-    
-    //多条件分页查询(rfy)
+public interface HouseDetailProService {
+	//多条件分页查询(rfy)
     public List<HouseDetailPro> queryPage(HouseDetailProQuery query);
     //查询总条数(rfy)
     public int queryCountPage(HouseDetailProQuery query);
-    
-    
 }
